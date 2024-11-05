08637b61934a05186822d90263c5be1c5f51b4a8
ito ryo <electrooculogram@gmail.com>
2024-11-05 16:49:23 +0900

    fix index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 exports.handler = async (event) => {
+  const targetUrl = process.env.TARGET_URL;
+
+  if (!targetUrl) {
+    return {
+      statusCode: 500,
+      body: "Error: TARGET_URL is not set.",
+    };
+  }
+
   return {
     statusCode: 301,
     headers: {
-      Location: 'https://example.com',
+      Location: targetUrl,
     },
     body: '',
   };
